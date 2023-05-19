70708220d3ca978b655b9e1ab5db723a35677b5d
Emilien Breton <bricktech2000@gmail.com>
2023-05-19 15:34:07 -0400

    updated russian roulette implementation

--- russian-roulette.cpp
+++ russian-roulette.cpp
@@ -5,13 +5,13 @@
 
 
 void panic(uint32_t code) {
-    std::cout << "This program just broke so badly we'll play some russian roulette!" << std::endl;
+  std::cout << "This program just broke so badly we'll play some russian roulette!" << std::endl;
 
-    //if unlucky, cause a segmentation fault
-    std::srand(std::time(NULL));
-    if(std::rand() % 6 <= 4) *(int*)0 = 0;
+  //if unlucky, cause a segmentation fault
+  std::srand(std::time(NULL));
+  if(std::rand() % 6 <= 4) *(volatile int*)0 = 0;
 
-    //if lucky, print out the error code
-    std::cout << "Error code: " << "[0x" << std::hex << code << "]" << ". Please contact this app's developper." << std::endl;
-    exit(1);
+  //if lucky, print out the error code
+  std::cout << "Error code: " << "[0x" << std::hex << code << "]" << ". Please contact this app's developper." << std::endl;
+  exit(1);
 }
